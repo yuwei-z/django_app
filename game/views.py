3aecf45b32d0d2f580e651a3121f64a8c8a80ad4
yuwei-z <thuzhangyw@gmail.com>
2022-01-02 14:58:06 +0800

    add webpages

--- game/views.py
+++ game/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line2 = '<a href="play/">进入游戏界面</a>'
+    line3 = '<img src="https://img0.baidu.com/it/u=4035339947,1492935398&fm=26&fmt=auto" width=2000>'
+    return HttpResponse(line1 + line2 + line3)
+
+def play(request):
+    line1 = '<h1 style="text-align:center">游戏界面</h1>'
+    line2 = '<a href="">回到首页</a>'
+    line3 = '<img src="https://img0.baidu.com/it/u=418569545,82599050&fm=26&fmt=auto" width=2000>'
+    return HttpResponse(line1 + line2 + line3)
